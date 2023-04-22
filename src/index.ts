7f7d44dbd838fd73e9b39972b3d64e1fcc3bae26
Willian Souza <91621589+Williansouzh@users.noreply.github.com>
2023-04-22 12:15:06 -0300

    fix: add port

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ import * as express from "express"
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const server = express()
 
+const port = process.env.PORT || 3333
+
 server.get("/", (req, res) => {
   res.send("hola ")
 })
 
-server.listen(3000)
+server.listen(port)
