48372f747b80edca0672e38d0183281e5f8a2705
Willian Souza <91621589+Williansouzh@users.noreply.github.com>
2023-04-22 12:10:59 -0300

    fix: finalizing setup

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,8 @@ import * as express from "express"
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const server = express()
 
+server.get("/", (req, res) => {
+  res.send("hola ")
+})
+
 server.listen(3000)
